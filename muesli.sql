243fa63a7f926aa211f3e0508a963ce2bc4e6710
Ingmar <ingmar@lowack.com>
2022-05-16 11:40:49 +0200

    changed mueslii query

--- muesli.sql
+++ muesli.sql
@@ -415,7 +415,8 @@ CREATE TABLE public.tutorials (
     comment text,
     "time" character varying(7),
     date date,
-    is_special boolean NOT NULL
+    is_special boolean NOT NULL,
+    video_call int
 );
 
 
@@ -1298,3 +1299,34 @@ ALTER TABLE ONLY public.user_has_updated
 -- PostgreSQL database dump complete
 --
 
+ALTER TABLE tutorials
+ADD COLUMN video_call boolean;
+
+INSERT INTO users (
+  email,
+  first_name,
+  last_name,
+  password,
+  matrikel,
+  birth_date,
+  birth_place,
+  subject,
+  second_subject,
+  title,
+  is_admin,
+  is_assistant
+) VALUES (
+  'test@test.com',
+  'test',
+  'test',
+  -- password: test
+  '$argon2id$v=19$m=65536,t=2,p=1$Ac5/rZUiOVYTlsouxYq4sw$ekkpbPk1zhK8cyI+ZISzGKv4/mzp831LaianUK8Q1fc',
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  1,
+  0
+)
--- patchDB.sql
+++ patchDB.sql
@@ -1,2 +1,31 @@
 ALTER TABLE tutorials
 ADD COLUMN video_call boolean;
+
+INSERT INTO users (
+  email,
+  first_name,
+  last_name,
+  password,
+  matrikel,
+  birth_date,
+  birth_place,
+  subject,
+  second_subject,
+  title,
+  is_admin,
+  is_assistant
+) VALUES (
+  'test@test.com',
+  'test',
+  'test',
+  -- password: test
+  '$argon2id$v=19$m=65536,t=2,p=1$Ac5/rZUiOVYTlsouxYq4sw$ekkpbPk1zhK8cyI+ZISzGKv4/mzp831LaianUK8Q1fc',
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  1,
+  0
+)
